ec221ba55b9a6504453753c85f9a9dcc0e70f1ac
Lp0lp <lpborara@gmail.com>
2026-02-26 13:51:48 +0100

    Add header to initial itp func.

--- shaker/itp.py
+++ shaker/itp.py
@@ -1,11 +1,16 @@
 
 
-def write_initial_CGitp (resname, bead_names, bead_types, bead_charges):
+def write_initial_CGitp (resname, 
+                         bead_names, bead_types, bead_charges, 
+                         header=''):
+    
     '''Function to write the inital CG itp file.'''
     
     initial = open('initial_CG.itp', 'w')
+    initial.write(header)
+    initial.write('\n')
     initial.write('[ moleculetype ]\n')
-    initial.write('{}  1'.format(resname))
+    initial.write(f'{resname}  1')
     initial.write('\n')
     initial.write('[ atoms ]\n')
     initial.write('; nr type resnr residue atom cgnr charge mass\n')
