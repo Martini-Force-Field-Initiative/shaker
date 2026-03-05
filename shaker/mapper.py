cac0213e2a714162480016f5390588dc26331d1f
Lp0lp <lpborara@gmail.com>
2026-03-05 15:56:42 +0100

    Updated Mapper to no longer depend on gmx. 2x speed.

--- shaker/mapper.py
+++ shaker/mapper.py
@@ -1,8 +1,6 @@
 import MDAnalysis as md
-from importlib.resources import files
-import os
-import subprocess
 from pathlib import Path
+import numpy as np
 
 '''
 Functions and tools to process and map AA/QM trajectories to CG.
@@ -23,86 +21,112 @@ def size_from_name(bead_types):
         else:  # For all other cases
             extra_list.append('R')
     return extra_list
-                    
-def map_aa2cg (gro, xtc, resnames,
-               bead_assignments, bead_names,
-               gmx_loc='', 
-               outname='cg_mapped', outdir='.'):
-    '''Provide the universe along with the lists containing the resnames of eg the lipids,
-    the bead_assignements (which atoms goes into which bead), and a list of the bead_names'''
 
-    ## normalize paths
+
+def map_aa2cg(gro, xtc, resnames,
+             bead_assignments, bead_names,
+             outname="cg_mapped", outdir="."):
+    
+    '''
+    Map an atomistic trajectory to a coarse-grained representation.
+
+    This function constructs coarse-grained beads from atomistic coordinates by
+    computing the center of geometry of user-defined groups of atoms for each residue. 
+    The mapping is applied to every frame of the input trajectory, producing a 
+    coarse-grained trajectory and a representative structure.
+
+    Parameters
+    ----------
+    gro : str or Path
+        Atomistic structure file (.gro or .pdb) used to initialize the system.
+    xtc : str or Path
+        Atomistic trajectory file (.xtc or .trr) containing the coordinates
+        to be mapped.
+    resnames : list of str
+        Residue names to be mapped. Each entry must correspond to a mapping
+        definition in `bead_assignments` and `bead_names`.
+    bead_assignments : list of list of list of str
+        Atom-to-bead mapping definitions. For each residue type in `resnames`,
+        this contains a list of beads, where each bead is defined by a list of
+        atom names contributing to that bead.
+    bead_names : list of list of str
+        Names assigned to the coarse-grained beads for each residue type.
+        The number of bead names must match the number of bead definitions
+        in `bead_assignments`.
+    outname : str, optional
+        Base name of the output files. Default is "cg_mapped".
+    outdir : str or Path, optional
+        Directory where the mapped structure and trajectory will be written.
+        Created if it does not exist.
+
+    Notes
+    -----
+    - Bead coordinates are computed as the center of geometry (COG) of the
+      atoms assigned to each bead.
+    - The output trajectory contains one particle per bead in the order
+      defined by `resnames` and `bead_assignments`.
+    - The output structure corresponds to the first frame of the mapped
+      trajectory.
+    - Atom selections that do not match any atoms will raise a `ValueError`.
+    '''
+    
     gro = Path(gro).resolve()
     xtc = Path(xtc).resolve()
     outdir = Path(outdir).resolve()
-    
-    # Initializing the universe
-    u = md.Universe(gro,xtc)
-    
-    # Generating the mapping index files, for each bead in each residue.
-    # Not the fastest but allows differential weighing of beads.
+    outdir.mkdir(parents=True, exist_ok=True)
+
+    u = md.Universe(gro, xtc)
+
+    # Build bead AtomGroups in the exact order we want 
     bead_agg = []
-    for i, molecule in enumerate(resnames):
-        targets = u.select_atoms(f'resname {molecule}').residues
-        for j in range(len(targets)):
-            target = targets[j].atoms
-            for bead in bead_assignments[i]:
-                agg_whole = u.select_atoms('name empty')
+    out_resids = []
+    out_resnames = []
+    out_atomnames = []
+
+    for i, resname in enumerate(resnames):
+        for res in u.select_atoms(f"resname {resname}").residues:
+            atoms = res.atoms
+            for bead_idx, bead in enumerate(bead_assignments[i]):
+                agg_whole = u.select_atoms("name empty")
                 for atom in bead:
-                    agg = target.select_atoms(f'name {atom}')
+                    agg = atoms.select_atoms(f"name {atom}")
                     if len(agg) == 0:
-                        raise ValueError(f"Atom '{atom}' not found in resname '{molecule}' "
-                                         f"(resid {targets[j].resid})")
+                        raise ValueError(f"Atom '{atom}' not found in resname '{resname}' (resid {res.resid})")
                     agg_whole += agg
                 bead_agg.append(agg_whole)
-    with md.selections.gromacs.SelectionWriter('MappingIndex.ndx', mode='w') as ndx:
-        for idx, agg in enumerate(bead_agg):
-            ndx.write(agg)
 
-    ## Create fake AA structure where all atoms are hydrogens.
-    ## Turns Gromacs' c.o.m. into c.o.g.
-    for atom in u.atoms:
-        atom.name = 'H' ## WARNING. After this point any name based selection will be fucked.
-    u.atoms.write('all_hydrogen.gro')
-    
-    #Using gromacs and the fancy index we wrote map the AA simulation into CG
-    env = os.environ.copy()
-    env["GMX_MAXBACKUP"] = "-1"   # disable #file.1# backups
-    
-    groups_in = "\n".join(map(str, range(len(bead_agg)))) + "\n"
-    cmd_gro = [f"{gmx_loc}gmx", "traj",
-               "-f", gro, "-s", "all_hydrogen.gro",
-               "-n", "MappingIndex.ndx", "-com",
-               "-ng", str(len(bead_agg)),
-               "-oxt", f"{outdir}/{outname}.gro",]
-    
-    cmd_xtc = [f"{gmx_loc}gmx", "traj",
-               "-f", xtc, "-s", "all_hydrogen.gro",
-               "-n", "MappingIndex.ndx", "-com",
-               "-ng", str(len(bead_agg)),
-               "-oxt", f"{outdir}/{outname}.xtc",]
-    
-    with open("gmx_mapping.log", "w") as log:
-        subprocess.run(cmd_gro, input=groups_in,
-                       text=True, env=env, stdout=log,
-                       stderr=subprocess.STDOUT, check=True,)
-        subprocess.run(cmd_xtc, input=groups_in,
-                       text=True, env=env, stdout=log,
-                       stderr=subprocess.STDOUT, check=True,)
+                # metadata for output topology
+                out_resids.append(res.resid)
+                out_resnames.append(res.resname)
+                out_atomnames.append(bead_names[i][bead_idx])
 
-    ### Fix names in GRO for easier parameterization
-    names = []
-    for i, molecule in enumerate(resnames):
-        targets = u.select_atoms('resname {}'.format(molecule)).residues
-        names += bead_names[i]*len(targets)
+    n_beads = len(bead_agg)
 
-    with open(f"{outdir}/{outname}.gro") as f:
-        gro_file = f.readlines()
-    for idx, line in enumerate(gro_file[2:-1]):
-        nm = str(names[idx]).strip()   
-        if len(nm) > 5:
-            warnings.warn(f"GRO atom name '{nm}' > 5 chars; truncating.")
-        nm = f"{nm:>5}"[:5]            # right-align, fixed width 5
-        gro_file[2 + idx] = line[:10] + nm + line[15:]
-    with open(f"{outdir}/{outname}.gro", "w") as f:
-        f.writelines(gro_file)
+    # Create an empty CG Universe with n_beads atoms
+    cg = md.Universe.empty(
+        n_atoms=n_beads,
+        n_residues=n_beads,          
+        atom_resindex=np.arange(n_beads),
+        trajectory=True,)
+
+    cg.add_TopologyAttr("name", out_atomnames)
+    cg.add_TopologyAttr("resname", out_resnames)
+    cg.add_TopologyAttr("resid", out_resids)
+
+    # Fill coordinates frame-by-frame...
+    coords = np.zeros((n_beads, 3), dtype=np.float32)
+
+    out_gro = outdir / f"{outname}.gro"
+    out_xtc = outdir / f"{outname}.xtc"
+    
+    wrote_gro = False
+    with md.Writer(out_xtc.as_posix(), n_beads) as W:
+        for ts in u.trajectory:
+            for k, ag in enumerate(bead_agg):
+                ## we could add a com flag here.
+                coords[k] = ag.center_of_geometry()   
+            cg.atoms.positions = coords
+            W.write(cg.atoms)
+            if not wrote_gro:              # write gro for first frame.
+                cg.atoms.write(out_gro.as_posix())
+                wrote_gro = True
