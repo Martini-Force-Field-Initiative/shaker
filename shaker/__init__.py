1ab27a2d680e7ccfcc918c250f94e226714b0856
Pablo Cardona Perez <pablo.cardona_perez@ens-lyon.fr>
2026-03-09 17:00:35 +0100

    chore: Expose package version at runtime

--- shaker/__init__.py
+++ shaker/__init__.py
@@ -1,1 +1,4 @@
 from . import dihedral_fitting, itp, mapper, measurer, sasa, system_builders, vsite3, helper, plot, render, vmd_render
+
+from importlib.metadata import version
+__version__ = version("shaker")
